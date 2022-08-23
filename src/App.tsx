f52332486d9bbf6f19944087e963f97151f9e564
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-23 22:30:03 +0800

    add button styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Button from "./components/Button/Button";
+
 function App() {
-  return <div>asdasdsa</div>;
+  const handleClick = () => console.log("Click!");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Button onClick={handleClick}>Primary Button</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,22 @@
 import { ReactNode, MouseEventHandler } from "react";
+import styles from "./Button.module.scss";
 
 interface Props {
   children: ReactNode;
   onClick: MouseEventHandler;
+  className?: string;
+  size?: "lg" | "sm";
 }
 
-const Button = ({ children, onClick }: Props) => {
-  return <button onClick={onClick}>{children}</button>;
+const Button = ({ children, onClick, className, size = "sm" }: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles[`button-${size}`]} ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
