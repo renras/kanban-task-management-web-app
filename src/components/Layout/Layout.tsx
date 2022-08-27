4644d33be2a9eb90d1e792e554a4210f37ebc627
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-27 23:03:09 +0800

    add menu button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import styles from "./Layout.module.scss";
 import Button from "../Button/Button";
+import MenuIcon from "../../assets/icon-vertical-ellipsis.svg";
 
 interface Props {
   children: ReactNode;
@@ -12,6 +13,9 @@ const Layout = ({ children }: Props) => {
       <header className={styles.header}>
         <h1 className={styles.heading}>Platform Launch</h1>
         <Button onClick={() => console.log("Clicked")}>+ Add New Task</Button>
+        <button className={styles.menu}>
+          <img src={MenuIcon} alt="menu" />
+        </button>
       </header>
       <main>{children}</main>
     </div>
