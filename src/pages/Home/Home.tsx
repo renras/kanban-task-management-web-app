d0b52924d2471926ea96a456d8364c7b97f2fad6
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-28 17:08:22 +0800

    initialize grid in main content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import styles from "./Home.module.scss";
 
 const Home = () => {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div>TODO</div>
+      <div>DOING</div>
+      <div>DONE</div>
+    </div>
+  );
 };
 
 export default Home;
